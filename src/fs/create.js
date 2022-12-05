d9684089dfbbf85923bc901ce0470416f062bba5
Daniyar Seferov <daniyar.seferov@rockdevels.com>
2022-12-05 19:04:31 +0500

    feat: implement create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { open } from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+	const filename = `${process.cwd()}/src/fs/files/fresh.txt`;
+	const content = 'I am fresh and young';
+	const errorMessage = 'FS operation failed';
+	let filehandle = null;
+
+	try {
+		filehandle = await open(filename, 'wx');
+		await filehandle.write(content);
+	} catch (err) {
+		if (err.code === 'EEXIST') {
+			throw new Error(errorMessage);
+		}
+		throw err;
+	} finally {
+		await filehandle?.close();
+	}
 };
 
 await create();
