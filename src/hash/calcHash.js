fb6c89cc15c694b460beaf8be8d4bcbbd3a538fe
Daniyar Seferov <daniyar.seferov@rockdevels.com>
2022-12-06 02:12:48 +0500

    feat: implement SHA256 hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { open } from "fs/promises";
+import { createHmac } from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+	const sourceFile = `${process.cwd()}/src/hash/files/fileToCalculateHashFor.txt`;
+	const errorMessage = 'FS operation failed';
+	let filehandle = null;
+	const secret = 'asfegewb';
+	const hmac = createHmac('sha256', secret);
+
+	try {
+		filehandle = await open(sourceFile);
+
+		for await (const line of filehandle.readLines()) {
+			hmac.update(line);
+		}
+		console.log(`${hmac.digest('hex')}`);
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			throw new Error(errorMessage);
+		}
+		throw err;
+	} finally {
+		await filehandle?.close();
+	}
 };
 
 await calculateHash();
