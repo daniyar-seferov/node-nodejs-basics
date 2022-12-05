77ec7a54384643fa9ff4a2dc2e6bb930aa81b0d5
Daniyar Seferov <daniyar.seferov@rockdevels.com>
2022-12-05 20:37:28 +0500

    feat: implement copy directory

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { mkdir, readdir, copyFile, constants } from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+	const sourceDirectory = `${process.cwd()}/src/fs/files`;
+	const destinationDirectory = `${process.cwd()}/src/fs/files_copy`;
+	const errorMessage = 'FS operation failed';
+
+	try {
+		await mkdir(destinationDirectory);
+		const files = await readdir(sourceDirectory);
+		for (const file of files) {
+			await copyFile(`${sourceDirectory}/${file}`, `${destinationDirectory}/${file}`, constants.COPYFILE_EXCL);
+		}
+	} catch (err) {
+		if (err.code === 'EEXIST') {
+			throw new Error(errorMessage);
+		}
+		throw err;
+	}
 };
 
 await copy();
