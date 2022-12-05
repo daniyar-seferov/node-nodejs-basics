3f295513105e018144fb8f113c43fd9301bd6dd4
Daniyar Seferov <daniyar.seferov@rockdevels.com>
2022-12-05 23:31:32 +0500

    feat: implement env parse parameters

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+	const parameters = Object.entries(process.env).filter((val, index) => {
+		return val[0].includes("RSS_");
+	}).map((val, index) => {
+		return `${val[0]}=${val[1]}`;
+	}).join('; ');
+
+	console.log(parameters);
 };
 
 parseEnv();
