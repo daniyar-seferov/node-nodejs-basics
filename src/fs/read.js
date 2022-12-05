4a8772d15ebead81005062af9ce0b368c353e5f5
Daniyar Seferov <daniyar.seferov@rockdevels.com>
2022-12-05 22:49:26 +0500

    feat: implement read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { open } from "fs/promises";
+
 const read = async () => {
-    // Write your code here 
+	const sourceFile = `${process.cwd()}/src/fs/files/fileToRead.txt`;
+	const errorMessage = 'FS operation failed';
+	let filehandle = null;
+
+	try {
+		filehandle = await open(sourceFile);
+
+		for await (const line of filehandle.readLines()) {
+			console.log(line);
+		}
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			throw new Error(errorMessage);
+		}
+		throw err;
+	} finally {
+		await filehandle?.close();
+	}
 };
 
 await read();
