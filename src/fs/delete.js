56517d0a73acd3c7a720cb8d2029d06bbd6ce941
Daniyar Seferov <daniyar.seferov@rockdevels.com>
2022-12-05 22:22:33 +0500

    feat: implement delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { rm } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+	const sourceFile = `${process.cwd()}/src/fs/files/fileToRemove.txt`;
+	const errorMessage = 'FS operation failed';
+
+	try {
+		await rm(sourceFile);
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			throw new Error(errorMessage);
+		}
+		throw err;
+	}
 };
 
 await remove();
