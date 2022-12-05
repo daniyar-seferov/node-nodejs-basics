914b6cddac5eec5e01d3d504222709842a1f060b
Daniyar Seferov <daniyar.seferov@rockdevels.com>
2022-12-06 00:16:49 +0500

    feat: implement parses command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
 const parseArgs = () => {
-    // Write your code here 
+	let isParam = false;
+
+	const parameters = process.argv.filter((val) => {
+		if (val.startsWith('--')) {
+			isParam = true;
+			return true;
+		}
+		if (isParam) {
+			isParam = false;
+			return true;
+		}
+		return false;
+	}).map((val, index, parameters) => {
+		if (val.startsWith('--')) {
+			return `${val.slice(2)} is ${parameters[index + 1]}`;
+		}
+	}).filter(val => val).join(', ');
+
+	console.log(parameters);
 };
 
 parseArgs();
